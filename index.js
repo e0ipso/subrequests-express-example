df7a77f8862cb848cad8bcbaade8816d6fd20b69
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-12-10 17:14:30 +0100

    Use a JSON object in the response

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const bodyParser = require('body-parser');
 const customers = require('./routes/customers');
 const users = require('./routes/users');
 const { subrequestsRouterFactory } = require('subrequests-express');
+const JsonResponse = require('subrequests-json-merger');
 
 app.use((req, res, next) => {
   res.set('Content-Type', 'application/json');
   next();
 });
 
+const subrequestsRoute = '/subrequests';
+
+// Merge responses in a JSON object.
+app.all(subrequestsRoute, (req, res, next) => {
+  // Make sure that subrequests-json-merger merges responses using JSON.
+  req.subrequestsResponseMerger = JsonResponse;
+  next();
+});
+
 // Subrequests expects the body to be a string. Make sure we add it before the
 // body parser.
-app.use(subrequestsRouterFactory('/subrequests'));
+app.use(subrequestsRouterFactory(subrequestsRoute));
 
 // start body-parser configuration
 app.use(
